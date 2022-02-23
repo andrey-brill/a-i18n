20bff140521aaf980afc9477164b93e033563a92
Andrey Brill <andrey@a-brill.com>
2022-02-23 19:18:39 +0100

    Removed comments

--- index.js
+++ index.js
@@ -3,20 +3,6 @@ import { ErrorCodes } from './src/Errors.js';
 import { FullKey } from './src/FullKey.js';
 import { I18n } from './src/I18n.js';
 
-// const i18n = new I18n({
-//   directory: './tests/huge',
-//   autoExport: true,
-//   debug: true
-// });
-
-
-// i18n.load();
-
-// if (i18n.state.loaded) {
-//   console.log('Keys:' + i18n.state.keys.array.length);
-// }
-
-
 export { I18n, ErrorCodes, FullKey }
 
 export default I18n;
